6c261720e2b8285a60141193bffda7d4b548dd00
Naresh Jaipal <132568227+NareshJaipal@users.noreply.github.com>
2024-04-04 14:20:52 +0500

    updated Portfolio UI

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,14 +1,6 @@
-const navDropDownBtn = document.getElementById("navDropDownBtn");
-const menuItems = document.getElementById("menuItems");
-
-navDropDownBtn.addEventListener("click", () => {
-  if (navDropDownBtn.classList.contains("fa-bars")) {
-    navDropDownBtn.classList.remove("fa-bars");
-    navDropDownBtn.classList.add("fa-x");
-    menuItems.classList.toggle("active");
-  } else {
-    navDropDownBtn.classList.remove("fa-x");
-    navDropDownBtn.classList.add("fa-bars");
-    menuItems.classList.remove("active");
-  }
+$("#navDropDownBtn").click(function () {
+  $("#navDropDownBtn").toggleClass("fa-bars fa-x");
+  $("#menuItems").toggleClass("active");
 });
+
+const about = $("#about");
